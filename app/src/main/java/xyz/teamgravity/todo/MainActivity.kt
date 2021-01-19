66af3f5a571d35803840f7eb7fc6b4639c6d8bf3
Raheem <raheemideadev@gmail.com>
2021-01-19 19:13:52 +0500

    raheem

--- app/src/main/java/xyz/teamgravity/todo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/todo/MainActivity.kt
@@ -2,10 +2,15 @@ package xyz.teamgravity.todo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import xyz.teamgravity.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
