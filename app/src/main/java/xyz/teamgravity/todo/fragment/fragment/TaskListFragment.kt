124f23d6c04e05c13f4c7b877512c66a2bae7525
Raheem <raheemideadev@gmail.com>
2021-01-19 19:52:26 +0500

    raheem

---
+++ app/src/main/java/xyz/teamgravity/todo/fragment/fragment/TaskListFragment.kt
@@ -0,0 +1,25 @@
+package xyz.teamgravity.todo.fragment.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import xyz.teamgravity.todo.databinding.FragmentTaskListBinding
+
+class TaskListFragment : Fragment() {
+
+    private var _binding: FragmentTaskListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentTaskListBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
