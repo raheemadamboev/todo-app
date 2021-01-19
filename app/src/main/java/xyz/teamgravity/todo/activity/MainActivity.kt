e59db59a36bcfc0a7fa30aa55617e3052b55869a
Raheem <raheemideadev@gmail.com>
2021-01-19 20:45:24 +0500

    raheem

--- app/src/main/java/xyz/teamgravity/todo/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/todo/activity/MainActivity.kt
@@ -2,7 +2,11 @@ package xyz.teamgravity.todo.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import xyz.teamgravity.todo.R
 import xyz.teamgravity.todo.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
@@ -10,9 +14,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // find nav controller in activity
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // set up action bar back button
+        setupActionBarWithNavController(navController)
+    }
+
+    // back button in action bar
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
