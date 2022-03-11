160bad8ba634b78ad1abd53a9d2d4f12754ac5b0
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-11 12:38:18 +0500

    refactored injection

--- app/src/main/java/xyz/teamgravity/todo/injection/ApplicationModule.kt
+++ app/src/main/java/xyz/teamgravity/todo/injection/ApplicationModule.kt
@@ -10,27 +10,41 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import xyz.teamgravity.todo.data.local.TodoCallback
 import xyz.teamgravity.todo.data.local.TodoConst
+import xyz.teamgravity.todo.data.local.TodoDao
 import xyz.teamgravity.todo.data.local.TodoDatabase
+import xyz.teamgravity.todo.data.repository.TodoRepository
+import javax.inject.Provider
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object ApplicationModule {
 
-    @Singleton
     @Provides
-    fun provideMyDatabase(app: Application, callback: TodoCallback) =
+    @Singleton
+    fun provideTodoDatabase(app: Application, callback: TodoCallback): TodoDatabase =
         Room.databaseBuilder(app, TodoDatabase::class.java, TodoConst.NAME)
             .addMigrations()
             .addCallback(callback)
             .build()
 
-    @Singleton
     @Provides
-    fun provideTaskDao(db: TodoDatabase) = db.todoDao()
-
     @Singleton
-    @Provides
     @ApplicationScope
-    fun provideApplicationScope() = CoroutineScope(SupervisorJob())
+    fun provideApplicationScope(): CoroutineScope = CoroutineScope(SupervisorJob())
+
+    @Provides
+    @Singleton
+    fun provideTodoCallback(
+        db: Provider<TodoDatabase>,
+        @ApplicationScope applicationScope: CoroutineScope
+    ): TodoCallback = TodoCallback(db = db, scope = applicationScope)
+
+    @Provides
+    @Singleton
+    fun provideTodoDao(db: TodoDatabase): TodoDao = db.todoDao()
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(dao: TodoDao): TodoRepository = TodoRepository(dao)
 }
