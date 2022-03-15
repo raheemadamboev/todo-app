3e3dfacdca93b5cdf4ab95ab7980478c83214301
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-15 18:04:52 +0500

    change text field colors

--- app/src/main/java/xyz/teamgravity/todo/presentation/component/TodoTextField.kt
+++ app/src/main/java/xyz/teamgravity/todo/presentation/component/TodoTextField.kt
@@ -2,31 +2,30 @@ package xyz.teamgravity.todo.presentation.component
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import xyz.teamgravity.todo.R
+import xyz.teamgravity.todo.presentation.theme.SuperLightWhite
 
 @Composable
 fun TodoTextField(
     value: String,
     onValueChange: (value: String) -> Unit
 ) {
-    OutlinedTextField(
+    TextField(
         value = value,
         onValueChange = onValueChange,
         placeholder = { Text(text = stringResource(id = R.string.task_name)) },
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            focusedBorderColor = MaterialTheme.colors.primary,
-            unfocusedBorderColor = MaterialTheme.colors.primary
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = SuperLightWhite
         ),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 8.dp, start = 8.dp, end = 8.dp)
+            .padding(top = 8.dp, start = 8.dp, end = 8.dp),
     )
 }
