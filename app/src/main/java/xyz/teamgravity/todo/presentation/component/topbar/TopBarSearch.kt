41af9badf7d15bee97df4064650d136f2f2a7ec9
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-23 14:42:10 +0500

    SearchField and BackButton gets placed on top of each other in ActionBar (Material 3) #8
    Took 38 minutes

--- app/src/main/java/xyz/teamgravity/todo/presentation/component/topbar/TopBarSearch.kt
+++ app/src/main/java/xyz/teamgravity/todo/presentation/component/topbar/TopBarSearch.kt
@@ -1,17 +1,18 @@
 package xyz.teamgravity.todo.presentation.component.topbar
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import xyz.teamgravity.todo.R
-import xyz.teamgravity.todo.presentation.theme.Muli
-import xyz.teamgravity.todo.presentation.theme.White
 
 @Composable
 fun TopBarSearch(
@@ -19,30 +20,26 @@ fun TopBarSearch(
     onQueryChange: (query: String) -> Unit,
     onCancel: () -> Unit
 ) {
-    TopBarIconButton(
-        onClick = onCancel,
-        icon = Icons.Default.ArrowBack,
-        contentDescription = R.string.cd_back_button
-    )
-    OutlinedTextField(
-        value = query,
-        onValueChange = onQueryChange,
-        singleLine = true,
-        placeholder = {
-            Text(
-                text = stringResource(id = R.string.search),
-                color = White.copy(alpha = 0.4F),
-                fontFamily = Muli
-            )
-        },
-        colors = TextFieldDefaults.outlinedTextFieldColors(
-            unfocusedBorderColor = Color.Transparent,
-            focusedBorderColor = Color.Transparent,
-            cursorColor = White
-        ),
-        textStyle = TextStyle.Default.copy(
-            color = White,
-            fontFamily = Muli
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        TopBarIconButton(
+            onClick = onCancel,
+            icon = Icons.Default.ArrowBack,
+            contentDescription = R.string.cd_back_button
         )
-    )
+        OutlinedTextField(
+            value = query,
+            onValueChange = onQueryChange,
+            singleLine = true,
+            placeholder = {
+                Text(text = stringResource(id = R.string.search))
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                unfocusedBorderColor = Color.Transparent,
+                focusedBorderColor = Color.Transparent
+            )
+        )
+    }
 }
