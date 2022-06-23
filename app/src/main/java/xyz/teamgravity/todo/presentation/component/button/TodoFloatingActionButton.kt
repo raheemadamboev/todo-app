8cf7d8e1eb056fdeaa43b71b9198802ed3ccd184
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-23 17:45:03 +0500

    change colors of TodoFloatingActionButton.kt
    Took 1 minute

--- app/src/main/java/xyz/teamgravity/todo/presentation/component/button/TodoFloatingActionButton.kt
+++ app/src/main/java/xyz/teamgravity/todo/presentation/component/button/TodoFloatingActionButton.kt
@@ -3,6 +3,7 @@ package xyz.teamgravity.todo.presentation.component.button
 import androidx.annotation.StringRes
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
@@ -13,7 +14,11 @@ fun TodoFloatingActionButton(
     icon: ImageVector,
     @StringRes contentDescription: Int
 ) {
-    FloatingActionButton(onClick = onClick) {
+    FloatingActionButton(
+        onClick = onClick,
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = MaterialTheme.colorScheme.onPrimary
+    ) {
         Icon(
             imageVector = icon,
             contentDescription = stringResource(id = contentDescription),
