a2d0ec3caf9f7e04988b39175e888d16efe808a3
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-24 18:09:31 +0500

    Add "Support" screen #16 - defining different layouts
    Took 4 minutes

---
+++ app/src/main/java/xyz/teamgravity/todo/presentation/screen/support/SupportLandscapeScreen.kt
@@ -0,0 +1,10 @@
+package xyz.teamgravity.todo.presentation.screen.support
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SupportLandscapeScreen(
+    onBackButtonClick: () -> Unit
+) {
+
+}
--- app/src/main/java/xyz/teamgravity/todo/presentation/screen/support/SupportScreen.kt
+++ app/src/main/java/xyz/teamgravity/todo/presentation/screen/support/SupportScreen.kt
@@ -3,11 +3,16 @@ package xyz.teamgravity.todo.presentation.screen.support
 import androidx.compose.runtime.Composable
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import xyz.teamgravity.todo.presentation.component.misc.WindowInfo
+import xyz.teamgravity.todo.presentation.component.misc.rememberWindowInfo
 
 @Destination
 @Composable
 fun SupportScreen(
     navigator: DestinationsNavigator
 ) {
-
+    when (rememberWindowInfo().screenWidthInfo) {
+        WindowInfo.WindowType.Compact -> SupportPortraitScreen(onBackButtonClick = navigator::popBackStack)
+        else -> SupportLandscapeScreen(onBackButtonClick = navigator::popBackStack)
+    }
 }
