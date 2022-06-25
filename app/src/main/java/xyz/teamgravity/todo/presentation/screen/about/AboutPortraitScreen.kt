9ce2aa997333129e2ce4ba731be331c7ce285772
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-25 16:42:50 +0500

    Make company logo clickable and navigate to Google Store Page #21 - portrait
    Took 23 minutes

--- app/src/main/java/xyz/teamgravity/todo/presentation/screen/about/AboutPortraitScreen.kt
+++ app/src/main/java/xyz/teamgravity/todo/presentation/screen/about/AboutPortraitScreen.kt
@@ -1,16 +1,19 @@
 package xyz.teamgravity.todo.presentation.screen.about
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -21,6 +24,7 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
 import xyz.teamgravity.todo.BuildConfig
 import xyz.teamgravity.todo.R
+import xyz.teamgravity.todo.core.util.Helper
 import xyz.teamgravity.todo.presentation.component.text.TextPlain
 import xyz.teamgravity.todo.presentation.component.topbar.TopBar
 import xyz.teamgravity.todo.presentation.component.topbar.TopBarIconButton
@@ -29,6 +33,9 @@ import xyz.teamgravity.todo.presentation.component.topbar.TopBarIconButton
 fun AboutPortraitScreen(
     onBackButtonClick: () -> Unit
 ) {
+
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TopBar(
@@ -48,7 +55,7 @@ fun AboutPortraitScreen(
                 .fillMaxSize()
                 .padding(padding)
         ) {
-            val (appI, appNameT, appVersionT, companyI, developerT) = createRefs()
+            val (appI, appNameT, appVersionT, companyC) = createRefs()
 
             Image(
                 painter = painterResource(id = R.drawable.icon),
@@ -83,28 +90,31 @@ fun AboutPortraitScreen(
                         top.linkTo(appNameT.bottom, 10.dp)
                     }
             )
-            Image(
-                painter = painterResource(id = R.drawable.gravity),
-                contentDescription = stringResource(id = R.string.cd_company_logo),
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .constrainAs(companyI) {
-                        width = Dimension.value(100.dp)
-                        height = Dimension.value(20.dp)
+                    .clip(MaterialTheme.shapes.extraLarge)
+                    .clickable { Helper.viewGravityPage(context) }
+                    .padding(horizontal = 10.dp, vertical = 5.dp)
+                    .constrainAs(companyC) {
                         linkTo(start = parent.start, end = parent.end)
-                        bottom.linkTo(developerT.top, 5.dp)
-                    }
-            )
-            Text(
-                text = stringResource(id = R.string.raheem),
-                style = MaterialTheme.typography.bodyMedium,
-                fontSize = 12.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .constrainAs(developerT) {
-                        width = Dimension.matchParent
                         bottom.linkTo(parent.bottom, 16.dp)
                     }
-            )
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.gravity),
+                    contentDescription = stringResource(id = R.string.cd_company_logo),
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(20.dp)
+                )
+                Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    text = stringResource(id = R.string.raheem),
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 12.sp,
+                )
+            }
         }
     }
 }
