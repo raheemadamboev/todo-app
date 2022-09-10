f69d5d7024ffb7002db03184fee430e31379dba1
raheemadamboev <vividricemaryandmax@gmail.com>
2022-09-10 17:05:28 +0500

    proper way of checking for orientation
    Took 2 minutes

--- app/src/main/java/xyz/teamgravity/todo/presentation/screen/support/SupportScreen.kt
+++ app/src/main/java/xyz/teamgravity/todo/presentation/screen/support/SupportScreen.kt
@@ -1,10 +1,10 @@
 package xyz.teamgravity.todo.presentation.screen.support
 
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalConfiguration
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import xyz.teamgravity.todo.presentation.component.misc.WindowInfo
-import xyz.teamgravity.todo.presentation.component.misc.rememberWindowInfo
 import xyz.teamgravity.todo.presentation.navigation.MainNavGraph
 
 @MainNavGraph
@@ -13,8 +13,8 @@ import xyz.teamgravity.todo.presentation.navigation.MainNavGraph
 fun SupportScreen(
     navigator: DestinationsNavigator
 ) {
-    when (rememberWindowInfo().screenWidthInfo) {
-        WindowInfo.WindowType.Compact -> SupportPortraitScreen(onBackButtonClick = navigator::popBackStack)
+    when (LocalConfiguration.current.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> SupportPortraitScreen(onBackButtonClick = navigator::popBackStack)
         else -> SupportLandscapeScreen(onBackButtonClick = navigator::popBackStack)
     }
 }
