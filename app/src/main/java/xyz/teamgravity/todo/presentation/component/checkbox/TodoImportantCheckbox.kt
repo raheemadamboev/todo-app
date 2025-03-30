5cccee2afa4cc676b9c276192230a0da0a285290
raheem <raheemideadev@gmail.com>
2025-03-31 03:46:34 +0500

    [1.2.0] Made text of TodoImportantCheckbox toggleable;

--- app/src/main/java/xyz/teamgravity/todo/presentation/component/checkbox/TodoImportantCheckbox.kt
+++ app/src/main/java/xyz/teamgravity/todo/presentation/component/checkbox/TodoImportantCheckbox.kt
@@ -2,14 +2,15 @@ package xyz.teamgravity.todo.presentation.component.checkbox
 
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.unit.dp
 import xyz.teamgravity.todo.R
 
@@ -20,7 +21,11 @@ fun TodoImportantCheckbox(
 ) {
     Row(
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.toggleable(
+            value = important,
+            role = Role.Checkbox,
+            onValueChange = onImportantChange
+        )
     ) {
         Checkbox(
             checked = important,
@@ -30,7 +35,7 @@ fun TodoImportantCheckbox(
             text = stringResource(id = R.string.important_task)
         )
         Spacer(
-            modifier = Modifier.width(8.dp)
+            modifier = Modifier.width(16.dp)
         )
     }
 }
